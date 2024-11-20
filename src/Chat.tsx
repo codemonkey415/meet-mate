24f160d62ddb31990a359b357bce8c90f9ba2c4d
codemonkey415 <lastsnowleopard415@gmail.com>
2024-11-20 02:11:08 -0500

    feat: build appointment dialog

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UserList from "./components/user-list/user-list";
 import InputChat from "./components/chat/input-chat";
 import ChatBox from "./components/chat/chat-box";
 import { getChatsAPI } from "./api/chat-api";
 import { fetchChats } from "./slice/chatSlice";
+import { Button } from "@headlessui/react";
+import { CalendarIcon } from "@heroicons/react/24/outline";
+import AppointmentDialog from "./components/appointment/appointment-dialog";
 
 const interviewee = process.env.REACT_APP_INTERVIEWEE_ID;
 
 export default function Chat() {
   const dispatch = useDispatch();
+  const [apptDlgOpen, setApptDlgOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const getChats = async () => {
@@ -19,15 +23,30 @@ export default function Chat() {
 
     getChats();
   }, []);
+
   return (
-    <div className="border border-gray-200 rounded-2xl grid grid-cols-7 w-full overflow-hidden">
-      <div className="col-span-2 border-r">
-        <UserList />
+    <div className="w-full space-y-4">
+      <Button
+        className="px-3 py-2 border border-gray-200 rounded-lg hover:bg-gray-50 flex items-center gap-2 font-semibold"
+        onClick={() => {
+          setApptDlgOpen(true);
+        }}
+      >
+        <CalendarIcon className="w-5" />
+        Schedule Appointment
+      </Button>
+
+      <div className="border border-gray-200 rounded-2xl grid grid-cols-7 w-full overflow-hidden">
+        <div className="col-span-2 border-r">
+          <UserList />
+        </div>
+        <div className="col-span-5 p-3">
+          <ChatBox />
+          <InputChat />
+        </div>
       </div>
-      <div className="col-span-5 p-3">
-        <ChatBox />
-        <InputChat />
-      </div>
+
+      <AppointmentDialog open={apptDlgOpen} handler={setApptDlgOpen} />
     </div>
   );
 }
---
+++ src/components/appointment/appointment-dialog.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+  Field,
+  Input,
+  Label,
+  Select,
+} from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+
+const interviewee = process.env.REACT_APP_INTERVIEWEE_ID;
+
+interface DialogBaseProps {
+  open: boolean;
+  handler: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppointmentDialog: React.FC<DialogBaseProps> = ({ open, handler }) => {
+  const { users } = useSelector((state: RootState) => state.user);
+  return (
+    <Dialog open={open} onClose={handler} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+          >
+            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle
+                    as="h2"
+                    className="text-lg font-semibold text-gray-900 mb-5 border-b pb-2"
+                  >
+                    Schedule Appointment
+                  </DialogTitle>
+                  <div className="space-y-4">
+                    <Field className="grid grid-cols-2 gap-4 items-center">
+                      <Label className="">User: </Label>
+                      <Select className="block w-full rounded-lg bg-white/5 py-1.5 px-3  border border-gray-200">
+                        {users
+                          .filter((user) => user.userId !== Number(interviewee))
+                          .map((user, key) => (
+                            <option value={user.userId}>{user.name}</option>
+                          ))}
+                      </Select>
+                    </Field>
+                    <Field className="grid grid-cols-2 gap-4 items-center">
+                      <Label className="">Interview Date: </Label>
+                      <Input
+                        type="datetime-local"
+                        className="block w-full rounded-lg bg-white/5 py-1.5 px-3  border border-gray-200"
+                      />
+                    </Field>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 border-t mt-5">
+              <button
+                type="button"
+                onClick={() => handler(false)}
+                className="inline-flex w-full min-w-16 justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+              >
+                Ok
+              </button>
+              <button
+                type="button"
+                data-autofocus
+                onClick={() => handler(false)}
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              >
+                Cancel
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default AppointmentDialog;
